9fc9c2a5ad3ca100b50b60642aa5435403e6293c
Vincius Von <vini.r.faria@hotmail.com>
2024-08-26 17:26:49 -0300

    criando strings e testando metodos

--- projeto-java/src/MinhaClasse.java
+++ projeto-java/src/MinhaClasse.java
@@ -1,8 +1,17 @@
 public class MinhaClasse {
     
     public static void main(String[] args) {
-        System.out.print ("Ola mundo!");
+    
+    String primeiroNome = "Vinicius";
+    String segundoNome = "Von";
+    String nomeCompleto= nomeCompleto(primeiroNome,segundoNome);
+    System.out.println(nomeCompleto);
 
-    }
+}
+
+public static String nomeCompleto (String primeiroNome, String segundoNome){
+    return "Resultado do metodo " + primeiroNome.concat(" ").concat(segundoNome);
+}
+
 
 }
